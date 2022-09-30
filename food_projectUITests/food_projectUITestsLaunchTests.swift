b9df71d04d517890e1f54d42dea07cd8d6a157fd
Luca Chaboissier <lucachaboissier@outlook.com>
2022-09-30 09:52:58 +0200

    initial commit

---
+++ food_projectUITests/food_projectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  food_projectUITestsLaunchTests.swift
+//  food_projectUITests
+//
+//  Created by Luca Chaboissier on 30/09/2022.
+//
+
+import XCTest
+
+class food_projectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
