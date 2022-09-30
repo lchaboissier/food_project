6f835a47b14404d0e73b25303ae12d5312a48fd0
Luca Chaboissier <lucachaboissier@outlook.com>
2022-09-30 11:19:34 +0200

    display menu added

---
+++ food_project/Controller/HomeController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeController.swift
+//  food_project
+//
+//  Created by Luca Chaboissier on 30/09/2022.
+//
+
+import Foundation
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
