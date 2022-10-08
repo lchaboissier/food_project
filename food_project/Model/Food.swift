c94d7f8a90e3965fb191a020de00cacff0458122
Luca Chaboissier <lucachaboissier@outlook.com>
2022-10-08 10:55:19 +0200

    Quartzcore import removed

--- food_project/Model/Food.swift
+++ food_project/Model/Food.swift
@@ -12,25 +12,7 @@ class Food {
     let content: String
     let tags: String
     let ingredients: [String]
-//    let ingredient2: String
-//    let ingredient3: String
-//    let ingredient4: String
-//    let ingredient5: String
-//    let ingredient6: String
-//    let ingredient7: String
-//    let ingredient8: String
-//    let ingredient9: String
-//    let ingredient10: String
-//    let ingredient11: String
-//    let ingredient12: String
-//    let ingredient13: String
-//    let ingredient14: String
-//    let ingredient15: String
-//    let ingredient16: String
-//    let ingredient17: String
-//    let ingredient18: String
-//    let ingredient19: String
-//    let ingredient20: String
+
     
     
     init(name: String, imgFood: String, content: String, tags: String, ingredients: [String])  {
@@ -39,24 +21,5 @@ class Food {
         self.content = content
         self.tags = tags
         self.ingredients = ingredients
-//        self.ingredient2 = ingredient2
-//        self.ingredient3 = ingredient3
-//        self.ingredient4 = ingredient4
-//        self.ingredient5 = ingredient5
-//        self.ingredient6 = ingredient6
-//        self.ingredient7 = ingredient7
-//        self.ingredient8 = ingredient8
-//        self.ingredient9 = ingredient9
-//        self.ingredient10 = ingredient10
-//        self.ingredient11 = ingredient11
-//        self.ingredient12 = ingredient12
-//        self.ingredient13 = ingredient13
-//        self.ingredient14 = ingredient14
-//        self.ingredient15 = ingredient15
-//        self.ingredient16 = ingredient16
-//        self.ingredient17 = ingredient17
-//        self.ingredient18 = ingredient18
-//        self.ingredient19 = ingredient19
-//        self.ingredient20 = ingredient20
     }
 }
